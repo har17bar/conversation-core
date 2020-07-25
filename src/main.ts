190f3b8438feae54f269358b77f918191805bc4b
Harut <har17bar@gmail.com>
2020-07-25 14:45:02 +0300

    chose platform-express as HTTP platform

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const PORT = 3000;
+  const logger = new Logger('bootstrap');
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  await app.listen(PORT);
+  logger.log(`Application listening on port ${PORT}`);
 }
 bootstrap();
