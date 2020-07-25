ba93bb672bd3d6ed2c18716c90f3c0128549fb58
Harut <har17bar@gmail.com>
2020-07-25 14:54:48 +0300

    setup cors and swagger as tool to document API and work with him

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const PORT = 3000;
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.enableCors(); // setup cors
+
+  const swaggerOptions = new DocumentBuilder() // setup swagger
+    .setTitle('Conversation')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerOptions);
+  SwaggerModule.setup('/docs', app, document);
+
   await app.listen(PORT);
   logger.log(`Application listening on port ${PORT}`);
 }
